e839582370a64f3e3acf1078e884853618074d4d
Valerus <juancavalerog@gmail.com>
2018-03-30 19:31:42 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 //requires
 var express = require('express');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 // inicializar variables
 var app = express();
 
+//body-parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 //conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -12,19 +21,14 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
   if (err) throw err;
 
   console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online' );
-})
-
-//rutas
-app.get('/', (req, res, next) => {
-
-  res.status(200).json({
-    ok: true,
-    mensaje: 'Peticion realizada correctamente'
-  });
 });
 
+//rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 //Escuchar peticiones
 app.listen(3000, () => {
   console.log('Express server puerto 3000');
-})
+});
